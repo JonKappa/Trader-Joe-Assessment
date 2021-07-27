6682da630d34d601b67cccd02dcb549bb2e70f4e
JonKappa <cappytwoj@gmail.com>
2021-07-27 14:09:45 -0400

    Trader Joe's 1

--- src/main/java/cd/rr/trader/Main.java
+++ src/main/java/cd/rr/trader/Main.java
@@ -1,9 +1,26 @@
 package cd.rr.trader;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        // Your solution here
+        StockPortfolio portfolio = new StockPortfolio(0);
+
+        portfolio.transfer(100.00);
+
+        portfolio.transfer(120.00);
+
+        portfolio.applyChange(5);
+
+        portfolio.transfer(-portfolio.value);
+
+        portfolio.applyChange(-10);
+
+        portfolio.transfer(portfolio.Joe);
+
+        portfolio.applyChange(5);
+        portfolio.transfer(-portfolio.value);
+        System.out.println(portfolio.Joe);
     }
 
 }
---
+++ src/main/java/cd/rr/trader/StockPortfolio.java
@@ -0,0 +1,36 @@
+package cd.rr.trader;
+
+public class StockPortfolio {
+    double value;
+    static int transferFee = 10;
+
+    StockPortfolio(double value){
+        this.value = value;
+    }
+
+
+    double Joe;
+    public double getValue() {
+        return value;
+    }
+
+    void transfer(double amount) {
+        if (amount > 0) {
+            Joe -= amount;
+            value = value + amount;
+            value -= transferFee;
+        }
+        else if (amount < 0) {
+            Joe = value - 10;
+            value = value + amount;
+        }
+
+        System.out.println(value);
+    }
+
+    void applyChange(double percentage) {
+        percentage = (percentage + 100) /100;
+        value = value * percentage;
+        System.out.println(value);
+    }
+}
